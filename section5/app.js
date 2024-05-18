0d2c02be8d8f024f85f0df9cf590436b9ab199d6
Satya Kalyan <satyakalyan5@gmail.com>
2024-05-18 17:09:23 -0600

    Learnt Express, Routing and Static File Serving

--- section5/app.js
+++ section5/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const path = require("path");
+const app = express();
 const bodyParser = require("body-parser");
+app.use(express.static(path.join(__dirname, "public")));
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-const app = express();
+const invalidRoutes = require("./routes/404");
 
 //the order matters here as well
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
+// app.use(invalidRoutes);
 app.use((req, res, next) => {
-	res.status(404).send("<h1>Page Not Found</h1>");
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
