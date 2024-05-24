43954762b5dfc15d66ded8a43b9880f449364e6d
Satya Kalyan <satyakalyan5@gmail.com>
2024-05-23 18:40:33 -0600

    Able to update the product

--- section5/routes/admin.js
+++ section5/routes/admin.js
@@ -12,6 +12,9 @@ router.get("/products", adminController.getProducts);
 
 router.post("/add-product", adminController.postAddProduct);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
+
 // module.exports = router;
 
 module.exports = router;
