4d8a7365bbaf0013e6e8b289492b1c46428e8d6a
Satya Kalyan <satyakalyan5@gmail.com>
2024-05-25 19:03:27 -0600

    Added Sequelize associations for Product and User

--- eCommApp/app.js
+++ eCommApp/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const path = require("path");
-const app = express();
+
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const app = express();
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+	User.findByPk(1)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,15 +33,27 @@ app.use(shopRoutes);
 
 // app.use(invalidRoutes);
 app.use(errorController.get404);
+Product.belongsTo(User, {
+	constraints: true,
+	onDelete: "CASCADE",
+});
+User.hasMany(Product);
 
 sequelize
+	// .sync({ force: true })
 	.sync()
 	.then((result) => {
+		return User.findByPk(1);
 		// console.log(result);
+	})
+	.then((user) => {
+		if (!user) return User.create({ name: "Max", email: "test@test.com" });
+		return user;
+	})
+	.then((user) => {
+		// console.log(user);
 		app.listen(3000);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
-
-// app.listen(3000);
